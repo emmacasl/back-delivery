6d60138e1b310e4f0a2a7df9053903bf3398f21c
Edgar Macas <edgarmacas1990@gmail.com>
2024-06-27 21:31:37 -0500

    add producto adin

--- delivery/admin.py
+++ delivery/admin.py
@@ -4,9 +4,16 @@ from django.contrib import admin
 
 from .models import Restaurante, Menu, Producto, Usuario, Pedido
 
+class ProductoAdmin(admin.ModelAdmin):
+    model = Producto
+    list_display = ['nombre', ]
+    search_fields = ['nombre',]
+    list_filter = ['nombre']
+
+
 admin.site.register(Restaurante)
 admin.site.register(Menu)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Usuario)
 admin.site.register(Pedido)
 
