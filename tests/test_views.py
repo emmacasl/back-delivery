1b47e89b584171be4e88fcf65cab458bb0e7ef8d
Edgar Macas <edgarmacas1990@gmail.com>
2024-08-06 20:39:13 -0500

    test

--- tests/test_views.py
+++ tests/test_views.py
@@ -8,5 +8,7 @@ def test_mymodel_list():
     client = APIClient()
     url = reverse('listado_menus')
     response = client.get(url)
+    print(response.data)
     assert response.status_code == 200
+    assert response['Content-Type'] == 'application/json'
     assert isinstance(response.json(), list)
